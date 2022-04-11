d9957d0a783456bbb01ddf15059fcca6f27e4d90
imranahmed1990 <imranmarin1990@gmail.com>
2022-04-12 02:31:05 +0600

    location redirect route setup done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/products' element={
+          <RequiredAuth>
+            <Products></Products>
+          </RequiredAuth>
+        }></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/orders' element={
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,25 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import app from '../../firebase.init';
 import useFirebase from '../../hooks/useFirebase';
+import { getAuth } from "firebase/auth";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
+
+const auth = getAuth(app);
 const Login = () => {
-    const { user, handleSignOut, googleSignIn } = useFirebase();
+    const [googleSignIn, user] = useSignInWithGoogle(auth)
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
     return (
         <div>
             <h2>Please Login!!</h2>
@@ -16,7 +32,7 @@ const Login = () => {
 
             </form>
 
-            <button onClick={googleSignIn}>Google Sign In</button>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
     );
 };
